4bf00f0b498c2ebaa1680ff54b326156ccab9bc3
nazoee <994804537al@gmail.com>
2025-11-16 05:41:03 +0900

    path fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/modelviewer',       // 页面、路由前缀
+  assetPrefix: '/modelviewer',    // 静态资源前缀
   typescript: {
     ignoreBuildErrors: true,
   },
